c5a231553c2102baabf8b23b9e37bf45c696133d
Julien Chavanton <jchavanton@gmail.com>
2018-07-07 14:19:50 +0000

    correct the server shell script

--- server.sh
+++ server.sh
@@ -1,1 +1,5 @@
-./voip_perf -p 5070 --trying --ringing --thread-count=1 -d 100
+./voip_perf --local-port=5070 \
+            --trying \
+            --ringing \
+            --thread-count=1 \
+            --duration=100 \
