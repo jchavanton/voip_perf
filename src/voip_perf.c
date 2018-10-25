99e0828ee31a1d401b5c0e5a12c8961cee794972
Julien Chavanton <jchavanton@gmail.com>
2018-10-25 15:46:26 -0700

    json config and random responses

--- src/voip_perf.c
+++ src/voip_perf.c
@@ -516,13 +516,19 @@ static pj_bool_t mod_call_on_rx_request(pjsip_rx_data *rdata) {
 		return PJ_TRUE;
 	}
 
-	/* Send 604/Ringing if needed */
-	//const pj_str_t reason = pj_str("Unable to create dialog");
-	//if (app.server.send_ringing) {
-	//	status = send_response(call->inv, rdata, 180, NULL, &has_initial);
-	//	if (status != PJ_SUCCESS)
-	//		return PJ_TRUE;
-	//}
+	/* json config responses */
+	int i;
+	for (i=0;i< app.server.responses_count ;i++) {
+
+		printf("%d %s [%d/100]\n", app.server.responses[i].code, app.server.responses[i].reason.ptr, app.server.responses[i].prob);
+		int x = rand()%100;
+		if (x < app.server.responses[i].prob) {
+			status = send_response(call->inv, rdata, app.server.responses[i].code,&app.server.responses[i].reason, &has_initial);
+			if (status != PJ_SUCCESS)
+				return PJ_TRUE;
+			return PJ_TRUE;
+		}
+	}
 
 	/* Send 180/Ringing if needed */
 	if (app.server.send_ringing) {
