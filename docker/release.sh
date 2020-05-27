391ec57d79201639101d9f1c2cd31b2b19b4f71e
Julien Chavanton <jchavanton@gmail.com>
2020-05-27 11:54:00 -0700

    small release improvements

---
+++ docker/release.sh
@@ -0,0 +1,5 @@
+docker tag voip_perf:latest jchavanton/voip_perf:0.5.3
+docker tag voip_perf:latest jchavanton/voip_perf:latest
+docker push jchavanton/voip_perf:0.5.3
+docker push jchavanton/voip_perf:latest
+
