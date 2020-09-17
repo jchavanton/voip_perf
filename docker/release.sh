227b9d4463b1e380de753fbd66df84db8b80c010
Julien Chavanton <jchavanton@gmail.com>
2020-09-17 14:07:38 -0700

    Bump version: 0.5.5 → 0.5.6

--- docker/release.sh
+++ docker/release.sh
@@ -1,5 +1,5 @@
-docker tag voip_perf:latest jchavanton/voip_perf:0.5.5
+docker tag voip_perf:latest jchavanton/voip_perf:0.5.6
 docker tag voip_perf:latest jchavanton/voip_perf:latest
-docker push jchavanton/voip_perf:0.5.5
+docker push jchavanton/voip_perf:0.5.6
 docker push jchavanton/voip_perf:latest
 
