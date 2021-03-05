41c1f9e5c06468aeaa9050318640543b12d45f44
Julien Chavanton <jchavanton@subspace.com>
2021-03-04 21:03:55 -0800

    Bump version: 0.5.7 → 0.5.8

--- docker/release.sh
+++ docker/release.sh
@@ -1,5 +1,5 @@
-docker tag voip_perf:latest jchavanton/voip_perf:0.5.7
+docker tag voip_perf:latest jchavanton/voip_perf:0.5.8
 docker tag voip_perf:latest jchavanton/voip_perf:latest
-docker push jchavanton/voip_perf:0.5.7
+docker push jchavanton/voip_perf:0.5.8
 docker push jchavanton/voip_perf:latest
 
